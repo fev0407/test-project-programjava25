d87286a1f209763ea19bc3f87ebbb36954e71dcf
fev0407 <Ekaterina.Fedorova@billing.ru>
2015-10-14 11:35:24 +0300

    ДЗ 5
    ДЗ 5

--- src/com/example/tests/GroupCreateTest.java
+++ src/com/example/tests/GroupCreateTest.java
@@ -1,5 +1,9 @@
 package com.example.tests;
 
+import java.util.Collections;
+import java.util.List;
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 
 public class GroupCreateTest extends TestBase{
@@ -8,23 +12,54 @@ public class GroupCreateTest extends TestBase{
   public void testNonEmptyGroupCreation() throws Exception {
 	  app.getNavigationHelper().openMainPage();
       app.getNavigationHelper().gotoGroupPage();
+      
+      // save old state
+      List<GroupData> oldList = app.getGroupHelper().getGroups();
+      
+      //actions
       app.getGroupHelper().initGroupCreation();
       GroupData group = new GroupData();
-      group.name = "my first group";
-      group.header = "header ek";
-      group.footer = "footer ek";
+      group.name = "aaa";
+      group.header = "header ek2";
+      group.footer = "footer ek2";
 	  app.getGroupHelper().fillGroupForm(group);
       app.getGroupHelper().submitGroupCreation();
       app.getGroupHelper().returnToGroupPage();
+      
+      //save new state
+      List<GroupData> newList = app.getGroupHelper().getGroups();
+      
+      // compare states
+      //assertEquals(newList.size(), oldList.size() + 1);
+      
+      oldList.add(group);
+      Collections.sort(oldList);
+      assertEquals(newList, oldList);    
+      
   }
   
   @Test
   public void testEmptyGroupCreation() throws Exception {
 	  app.getNavigationHelper().openMainPage();
       app.getNavigationHelper().gotoGroupPage();
+      
+      // save old state
+      List<GroupData> oldList = app.getGroupHelper().getGroups();
+      
+      // actions
+      GroupData group = new GroupData("", "", "");
       app.getGroupHelper().initGroupCreation();
-      app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
+      app.getGroupHelper().fillGroupForm(group);
       app.getGroupHelper().submitGroupCreation();
       app.getGroupHelper().returnToGroupPage();
+      
+      //save new state
+      List<GroupData> newList = app.getGroupHelper().getGroups();
+      
+      // compare states
+            
+      oldList.add(group);
+      Collections.sort(oldList);
+      assertEquals(newList, oldList);  
   }
 }
--- src/com/example/tests/GroupModifiationTests.java
+++ src/com/example/tests/GroupModifiationTests.java
@@ -1,5 +1,10 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class GroupModifiationTests extends TestBase {
@@ -8,12 +13,28 @@ public class GroupModifiationTests extends TestBase {
 	public void modifySomeGroup() {
 		app.getNavigationHelper().openMainPage();
 	    app.getNavigationHelper().gotoGroupPage();
-		app.getGroupHelper().initGroupModification(1);
+	    
+	    // save old state
+	    List<GroupData> oldList = app.getGroupHelper().getGroups();
+	    
+	    //actions
+	    app.getGroupHelper().initGroupModification(0);
 		GroupData group = new GroupData();
 		group.name = "new name";
 		app.getGroupHelper().fillGroupForm(group);
 		app.getGroupHelper().submitGroupModification();
 		app.getGroupHelper().returnToGroupPage();
+		
+		//save new state
+	    List<GroupData> newList = app.getGroupHelper().getGroups();
+	      
+	    // compare states
+	    //assertEquals(newList.size(), oldList.size() + 1);
+	      
+	     oldList.remove(0);
+	     oldList.add(group);
+	     Collections.sort(oldList);
+	     assertEquals(newList, oldList);
 	}
 	
 }
